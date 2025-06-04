08d3e51f87657e56473cc62255a39735dcb9f315
JChoy <jchoyeclipse@gmail.com>
2025-06-04 17:26:53 +0900

    feat: make use redeployable salt

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,7 +5,7 @@ import {Script, console} from "forge-std/Script.sol";
 import {Wrapped6909Factory} from "../src/Wrapped6909Factory.sol";
 
 interface ICreateX {
-    function deployCreate2(bytes memory initCode) external payable returns (address newContract);
+    function deployCreate2(bytes32 salt, bytes memory initCode) external payable returns (address newContract);
 }
 
 contract DeployScript is Script {
@@ -19,8 +19,15 @@ contract DeployScript is Script {
         vm.stopBroadcast();
     }
 
+    function _encodeRedeployableSalt(address msgSender, bytes11 salt) internal pure returns (bytes32 encodedSalt) {
+        assembly {
+            encodedSalt := or(shl(96, msgSender), shr(168, shl(168, salt)))
+        }
+    }
+
     function deploy() public broadcast {
-        address deployed = ICreateX(CREATEX_ADDRESS).deployCreate2(type(Wrapped6909Factory).creationCode);
+        bytes32 salt = _encodeRedeployableSalt(msg.sender, bytes11(0));
+        address deployed = ICreateX(CREATEX_ADDRESS).deployCreate2(salt, type(Wrapped6909Factory).creationCode);
         console.log("Deployed to", deployed);
     }
 }
